2166d1e091b4e11b118cc57b68596cfda33ef646
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-10-23 21:59:09 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Schaltwert += -5
+    basic.showNumber(Schaltwert)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    Schaltwert += 5
+    basic.showNumber(Schaltwert)
+})
+let Schaltwert = 0
+basic.showString("ft-trockner")
 let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 pins.digitalWritePin(DigitalPin.P0, 1)
+Schaltwert = 60
 loops.everyInterval(400, function () {
     _4digit.show(pins.analogReadPin(AnalogPin.P2))
-    if (pins.analogReadPin(AnalogPin.P2) < 60) {
+    if (pins.analogReadPin(AnalogPin.P2) < Schaltwert) {
         basic.setLedColor(0xff0000)
         motors.motorCommand(MotorCommand.Break)
     } else {
