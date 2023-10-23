1dda5b48c1cba947f96b66385054191c063ed3c6
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-10-23 21:49:00 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+pins.digitalWritePin(DigitalPin.P0, 1)
+loops.everyInterval(400, function () {
+    _4digit.show(pins.analogReadPin(AnalogPin.P2))
+    if (pins.analogReadPin(AnalogPin.P2) < 60) {
+        basic.setLedColor(0xff0000)
+        motors.motorCommand(MotorCommand.Break)
+    } else {
+        basic.setLedColor(0x00ff00)
+        motors.motorPower(95)
+    }
 })
